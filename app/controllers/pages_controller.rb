c323737e88b4610e0bbe4644908e43498a5ed2ba
Anthony Pearce <awpearce@outlook.com>
2016-12-18 01:23:55 -0800

    Connected the three home page postings to the three most recent blog posts.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    @last = Article.last
+    @second_last = Article.offset(1).last
+    @third_last = Article.offset(2).last
   end
 
   def about
